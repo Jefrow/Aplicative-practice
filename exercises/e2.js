68ae62894a00049b1ba923b143d2e6e8edb81345
Jethro Coma <jethrocoma@gmail.com>
2023-01-14 18:58:21 -0800

    complete excercise/es2.js

--- exercises/e2.js
+++ exercises/e2.js
@@ -6,6 +6,13 @@ import { data } from "../data/data";
 
 export function getAsteroidNames(data) {
   // Your code goes here...
+  const asteroids = data.asteroids; 
+
+  const asteroidNames = asteroids.map((asteroid) => {
+    return asteroid.name;
+  })
+
+  return asteroidNames;
 }
 
 
